04f7b4791480ef379a1bb8b4c7b180e704a53847
Chukwudi <ikenwakochukwudi@gmail.com>
2022-12-21 04:23:42 -0800

    added puppet file for adcanced task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,45 @@
+# File that installs nginx, and sets it up to have a custome header
+$doc_root = "/var/www/html"
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => "installed",
+  require => Exec['apt-get update'],
+}
+
+file { $doc_root:
+  ensure => "directory",
+  owner  => "www-data",
+  group  => "www-data",
+  mode   => "0644",
+}
+
+file { "{$doc_root}/index.html":
+  ensure  => "present",
+  content => "Hello World!",
+  require => File[$doc_root],
+}
+
+file { "/etc/nginx/sites-available/default":
+  ensure => "present",
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /redirect_me {
+      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+  }",
+  notify => Service['nginx'],
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
